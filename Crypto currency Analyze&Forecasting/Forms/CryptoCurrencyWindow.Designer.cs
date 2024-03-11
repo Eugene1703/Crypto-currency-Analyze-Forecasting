dd4437f9e72726b8ca2b218a9662fc6680918f73
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2024-03-11 15:04:35 +0200

    project initial

---
+++ Crypto currency Analyze&Forecasting/Forms/CryptoCurrencyWindow.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Crypto_currency_Analyze_Forecasting
+{
+    partial class CryptoCurrencyWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CryptoCurrencyWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "CryptoCurrencyWindow";
+            this.Text = "CryproCurrencyWindow";
+            this.Load += new System.EventHandler(this.CryptoCurrencyWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Crypto currency Analyze&Forecasting/Forms/CryptoCurrencyWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crypto_currency_Analyze_Forecasting
+{
+    public partial class CryptoCurrencyWindow : Form
+    {
+        public CryptoCurrencyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CryptoCurrencyWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
