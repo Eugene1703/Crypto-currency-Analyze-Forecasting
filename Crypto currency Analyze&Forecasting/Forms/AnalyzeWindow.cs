334782291931fe1d2133d8c6a72788ae1dd73c3a
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2024-03-11 15:08:22 +0200

    forms add

---
+++ Crypto currency Analyze&Forecasting/Forms/AnalyzeWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crypto_currency_Analyze_Forecasting.Forms
+{
+    public partial class AnalyzeWindow : Form
+    {
+        public AnalyzeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalyzeWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
